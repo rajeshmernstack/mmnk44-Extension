009570a4122f7aea3a9e33e11dd7585767f22c63
Rajesh Kumar <rajesh.bcsf18@iba-suk.edu.pk>
2022-09-24 21:12:44 +0500

    Extension Done

--- content-script.js
+++ content-script.js
@@ -1,5 +1,5 @@
 
-setInterval(() => {
+setTimeout(() => {
     chrome.storage.local.get("kindledescription", function (retrieved_data) {
         if (retrieved_data.kindledescription != undefined) {
             document.querySelectorAll('iframe')[0].contentWindow.document.children[0].children[1].innerText = retrieved_data.kindledescription;
--- popup.js
+++ popup.js
@@ -1,20 +1,43 @@
 document.getElementById("license-key-container").classList.add('d-none');
-document.getElementById("main-container").classList.add("d-none");
+document.getElementById("main-container").classList.add('d-none');
 
 chrome.storage.local.get("licensekey", function (retrieved_data) {
     if(retrieved_data.licensekey == undefined) {
+        document.getElementById('verify-license-key').classList.add('d-none');
         document.getElementById("license-key-container").classList.remove("d-none");
     }else{
-        document.getElementById("main-container").classList.remove("d-none");
+
+        validateLicenseKeyAtStartup(retrieved_data.licensekey);
     }
     // document.getElementById("saved-description").innerText = retrieved_data.kindledescription;
 });
+function validateLicenseKeyAtStartup(key) {
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://mmnk44.mernweb.com/wp-json/lmfwc/v2/licenses/validate/${key}`);
+    xhr.setRequestHeader("Authorization", "Basic " + btoa("ck_523808b4183d8657861a1758ae0d3ccc7142f0bd:cs_99f70dc68b478e75a52eaebe15cc67bc2c93f062"));
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            let myResponse = JSON.parse(xhr.response);
+            document.getElementById("license-key-container").classList.remove("d-none");
+            document.getElementById('verify-license-key').classList.add('d-none');
+        } else {
+            let myResponse = JSON.parse(xhr.response);
+            if (myResponse.success != undefined) {
+                if (myResponse.success) {
+                    document.getElementById('verify-license-key').classList.add('d-none');
+                    document.getElementById("main-container").classList.remove("d-none");
+                }
+            }
+        }
+    };
+}
 
 chrome.storage.local.get("kindledescription", function (retrieved_data) {
-    if (retrieved_data.kindledDescription == undefined) {
+    if (retrieved_data.kindledescription == undefined) {
         document.getElementById("saved-description").innerText = "No Any Saved Description Found";
     } else {
-
         document.getElementById("saved-description").innerText = retrieved_data.kindledescription;
     }
 });
@@ -39,39 +62,41 @@ document.getElementById('save-description-btn').addEventListener('click', () => 
 
 document.getElementById("licensekey-submit-btn").addEventListener('click', () => {
     let licensekey = document.getElementById("licensekey").value;
+    validateLicenseKey(licensekey)
+})
+
+
+
+
+function validateLicenseKey(key) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://mmnk44.mernweb.com/wp-json/lmfwc/v2/licenses/validate/${licensekey}`);
+    xhr.open('GET', `https://mmnk44.mernweb.com/wp-json/lmfwc/v2/licenses/validate/${key}`);
     xhr.setRequestHeader("Authorization", "Basic " + btoa("ck_523808b4183d8657861a1758ae0d3ccc7142f0bd:cs_99f70dc68b478e75a52eaebe15cc67bc2c93f062"));
     xhr.send();
     xhr.onload = function () {
-        if (xhr.status != 200) { // analyze HTTP status of the response
-            // alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+        if (xhr.status != 200) {
             let myResponse = JSON.parse(xhr.response);
-
             document.getElementById('response-message').innerText = ""+myResponse.message;
-        } else { // show the result
-            // alert(`Done, got ${xhr.response.length} bytes`); // response is the server response
+        } else {
             let myResponse = JSON.parse(xhr.response);
             if (myResponse.success != undefined) {
                 if (myResponse.success) {
                     document.getElementById('response-message').innerText = "License KEY OK"
                     chrome.storage.local.set({
-                        licensekey: licensekey
+                        licensekey: key
                     }, function () {
                         document.getElementById('response-message').innerText = "License KEY Saved Successfully"
                         location.reload()
                     });
                 }
             }
-            // alert(xhr.response.status); // response is the server response
         }
     };
     xhr.onprogress = function (event) {
         if (event.lengthComputable) {
-            // alert(`Received ${event.loaded} of ${event.total} bytes`);
             document.getElementById('response-message').innerText = "Loading";
         } else {
-            // alert(`Received ${event.loaded} bytes`); // no Content-Length
+            document.getElementById('response-message').innerText = "Loading";
         }
 
     };
@@ -79,4 +104,4 @@ document.getElementById("licensekey-submit-btn").addEventListener('click', () =>
         document.getElementById('response-message').innerText = "Error While Validating License Key";
 
     };
-})
+}
